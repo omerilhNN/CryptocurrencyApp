f0976b9d8cdc2cbc1d7f81a2e8338837f1e959c3
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2023-08-24 14:44:40 +0300

    Screen,TeamListItem

---
+++ app/src/main/java/com/omrilhn/cryptocurrencyapp/presentation/coin_detail/components/TeamListItem.kt
@@ -0,0 +1,11 @@
+package com.omrilhn.cryptocurrencyapp.presentation.coin_detail.components
+
+import androidx.compose.runtime.Composable
+import com.omrilhn.cryptocurrencyapp.data.remote.dto.TeamMember
+
+@Composable
+fun TeamListItem(
+    teamMember: TeamMember
+){
+
+}
