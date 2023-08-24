d85c30f5aef6b187b3dd62f92d049515c33aa911
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2023-08-24 17:27:33 +0300

    Finalized

--- app/src/main/java/com/omrilhn/cryptocurrencyapp/domain/use_case/get_coins/GetCoinsUseCase.kt
+++ app/src/main/java/com/omrilhn/cryptocurrencyapp/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -3,6 +3,7 @@ package com.omrilhn.cryptocurrencyapp.domain.use_case.get_coins
 import com.omrilhn.cryptocurrencyapp.common.Resource
 import com.omrilhn.cryptocurrencyapp.data.remote.dto.toCoin
 import com.omrilhn.cryptocurrencyapp.domain.model.Coin
+import com.omrilhn.cryptocurrencyapp.domain.model.CoinDetail
 import com.omrilhn.cryptocurrencyapp.domain.repository.CoinRepository
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
@@ -14,15 +15,15 @@ class GetCoinsUseCase @Inject constructor(
 ) {
     operator fun invoke() : kotlinx.coroutines.flow.Flow<Resource<List<Coin>>> = flow {
         try {
-            emit(Resource.Loading())
+            emit(Resource.Loading<List<Coin>>())
             val coins = repository.getCoins().map{it.toCoin()} //Map: Convert every element of the list to type that you want
-            emit(Resource.Success(coins))
+            emit(Resource.Success<List<Coin>>(coins))
 
         }catch( e: HttpException){
-            emit(Resource.Error(e.localizedMessage ?: "An unexpected error occured"))
+            emit(Resource.Error<List<Coin>>(e.localizedMessage ?: "An unexpected error occured"))
 
         }catch (e:IOException){
-            emit(Resource.Error("Couldn't reach server. Check your internet connection."))
+            emit(Resource.Error<List<Coin>>("Couldn't reach server. Check your internet connection."))
         }
     }
 }
--- app/src/main/java/com/omrilhn/cryptocurrencyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/omrilhn/cryptocurrencyapp/presentation/MainActivity.kt
@@ -6,12 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.omrilhn.cryptocurrencyapp.presentation.coin_detail.CoinDetailScreen
+import com.omrilhn.cryptocurrencyapp.presentation.coin_list.CoinListScreen
 import com.omrilhn.cryptocurrencyapp.presentation.theme.CryptocurrencyAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +28,31 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ){
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ){
+                            CoinListScreen(navController)
+                        }
+                        composable(route = Screen.CoinDetailScreen.route + "/{coinId}")
+                        {
+                            CoinDetailScreen()
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CryptocurrencyAppTheme {
-        Greeting("Android")
+
     }
 }
